a1574e2b5014683af36413bb8e73fdd7b0c06a90
woadud Akand <aazztech12@gmail.com>
2019-06-23 10:38:36 +0600

    Updates

---
+++ precache-manifest.b8afc1beff05075b3468899e6d56c356.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a0a3150a7075890ad1747eac798676",
+    "url": "/basic_react_redux/index.html"
+  },
+  {
+    "revision": "0eae9d847dc6d6e6fa5b",
+    "url": "/basic_react_redux/static/js/2.95bb1e4b.chunk.js"
+  },
+  {
+    "revision": "30372406b87c6f7c6efc",
+    "url": "/basic_react_redux/static/js/main.0ca9a4cc.chunk.js"
+  },
+  {
+    "revision": "fa8848b2a748a859f983",
+    "url": "/basic_react_redux/static/js/runtime~main.25003c45.js"
+  }
+]);
--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-// Action Creator
-export const selectSong = song => {
-    // return action
-    return {
-        type: 'SONG_SELECTED',
-        payload: song
-    }
-}
